5e80db4ffcc41e6454f57c276b1c7bfcf8191809
Sebastian Netsch <sebastian.netsch@hotmail.com>
2022-03-23 19:24:29 +0100

    Finish Dodge the Creeps!-project

---
+++ Dodge the Creeps!/Main.gd
@@ -0,0 +1,58 @@
+extends Node
+
+export (PackedScene) var mob_scene
+var score
+
+
+func _ready():
+	randomize()
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_MobTimer_timeout():
+	# Create a new instance of mob scene
+	var mob = mob_scene.instance()
+	
+	# Choose a spawn location on Path2D
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+	
+	# Set the mobs direction perpendicular to the path direction
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	# Set the mobs location to a random location
+	mob.position = mob_spawn_location.position
+	
+	# Add some randomness to the direction
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# Set mob velocity
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
